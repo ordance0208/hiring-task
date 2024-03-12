74c4ffc0dd314b59893dafa16afc82a2fa87a2d8
Ordan Gramatov <ordance01@yahoo.com>
2024-03-12 20:38:54 +0100

    Created new component for combinging audio

---
+++ client/src/components/CombineRecordings.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="combine-recordings">
+    <div><button :disabled="!combineAvailable">Combine</button></div>
+    <div><button>Play Combined Audio</button></div>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    combineAvailable: Boolean
+  })
+</script>
