0ed8d6c61bdb46076a0c1d8e381ac5fc8da1551b
Ordan Gramatov <ordance01@yahoo.com>
2024-03-14 19:13:58 +0100

    Added middleware and router

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import audioRouter from './routers/audio.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/audio', audioRouter);
 
 app.listen(8000, () => {
   console.log('Server running on port 8000');
