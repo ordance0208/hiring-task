37521a87f2148c058f9dae1b2e860ccbef32fb74
Ordan Gramatov <ordance01@yahoo.com>
2024-03-15 16:10:23 +0100

    Refactored component

--- client/src/components/CombineRecordings.vue
+++ client/src/components/CombineRecordings.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="combine-recordings">
-    <div><button :disabled="!combineAvailable">Combine</button></div>
-    <div><button>Play Combined Audio</button></div>
+    <div>
+      <button @click="handleCombineAudio" :disabled="!combineAudioDisabled">
+        Combine
+      </button>
+    </div>
+    <div>
+      <button @click="handlePlayCombinedAudio" :disabled="!combinedAudio">
+        Play Combined Audio
+      </button>
+    </div>
+    <audio ref="audioElement"></audio>
   </div>
 </template>
 
 <script setup>
-  const props = defineProps({
-    combineAvailable: Boolean
-  })
+import { computed, ref } from 'vue';
+import { combineAudio } from '../endpoints/audio';
+
+const props = defineProps({
+  firstAudio: Object,
+  secondAudio: Object,
+});
+
+const combinedAudio = ref(null);
+const audioElement = ref(null);
+const combineAudioDisabled = computed(() => props.firstAudio && props.secondAudio);
+
+const handlePlayCombinedAudio = () => {
+  if (audioElement.value.paused) {
+    audioElement.value.src = combinedAudio.value;
+    audioElement.value.play();
+  }
+};
+
+const handleCombineAudio = async () => {
+  const formData = new FormData();
+
+  formData.append('audio1', props.firstAudio);
+  formData.append('audio2', props.secondAudio);
+  const data = await combineAudio(formData);
+
+  const url = URL.createObjectURL(data);
+  combinedAudio.value = url;
+};
 </script>
